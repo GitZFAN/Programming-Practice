c198213d561b71a082e8d187971d93810f3b5785
fanZ <z_fan@outlook.com>
2020-07-15 20:07:04 +0800

    update JZ48 to JZ47.

---
+++ src/main/java/nowcode/Solution_JZ47.java
@@ -0,0 +1,9 @@
+package nowcode;
+
+public class Solution_JZ47 {
+    int Sum_Solution(int n) {
+        int sum = n;
+        boolean ans = (n > 0) && ((sum += Sum_Solution(n - 1)) > 0);
+        return sum;
+    }
+}
